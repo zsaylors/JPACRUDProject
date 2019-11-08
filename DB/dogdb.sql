25ce748b0322baa339fed124099200f0fd4e37b1
Zak Saylors <zsaylors11@gmail.com>
2019-11-08 15:01:48 -0700

    Stub DB and dog table - may change table later.

---
+++ DB/dogdb.sql
@@ -0,0 +1,48 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema dogdb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `dogdb` ;
+
+-- -----------------------------------------------------
+-- Schema dogdb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `dogdb` DEFAULT CHARACTER SET utf8 ;
+USE `dogdb` ;
+
+-- -----------------------------------------------------
+-- Table `dog`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `dog` ;
+
+CREATE TABLE IF NOT EXISTS `dog` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+SET SQL_MODE = '';
+DROP USER IF EXISTS groot@localhost;
+SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+CREATE USER 'groot'@'localhost' IDENTIFIED BY 'groot';
+
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'groot'@'localhost';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `dog`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `dogdb`;
+INSERT INTO `dog` (`id`, `name`) VALUES (1, 'Fred');
+
+COMMIT;
+
