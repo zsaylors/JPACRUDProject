f3f0ab65676f4234254390d28eb7bdd823314f4a
Zak Saylors <zsaylors11@gmail.com>
2019-11-10 23:05:35 -0700

    updated database

--- DB/memes.sql
+++ DB/memes.sql
@@ -48,8 +48,18 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `memesdb`;
-INSERT INTO `memes` (`id`, `top_quote`, `bottom_quote`, `img_url`, `font_color`, `generated_img`, `generated_data`, `animal`) VALUES (1, 'Not Today', 'I\'m tired', NULL, '#ffffff', NULL, NULL, NULL);
-INSERT INTO `memes` (`id`, `top_quote`, `bottom_quote`, `img_url`, `font_color`, `generated_img`, `generated_data`, `animal`) VALUES (2, 'cats', 'pushing', 'https://i.ytimg.com/vi/ccK3usCWmTo/maxresdefault.jpg', '#ffffff', NULL, NULL, NULL);
+INSERT INTO `memes` (`id`, `top_quote`, `bottom_quote`, `img_url`, `font_color`, `generated_img`, `generated_data`, `animal`) VALUES (1, NULL, NULL, 'https://memeswillsaveus.files.wordpress.com/2016/09/no_frisbee_dog.jpg', '#ffffff', NULL, NULL, 'dog');
+INSERT INTO `memes` (`id`, `top_quote`, `bottom_quote`, `img_url`, `font_color`, `generated_img`, `generated_data`, `animal`) VALUES (2, 'IT\'S A', 'GUINEA SAURUS REX!', 'https://i.ytimg.com/vi/yHCBx3_r2-I/hqdefault.jpg', '#60a9ff', NULL, NULL, 'other');
+INSERT INTO `memes` (`id`, `top_quote`, `bottom_quote`, `img_url`, `font_color`, `generated_img`, `generated_data`, `animal`) VALUES (3, NULL, NULL, 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS9lQrMXT7m33-EFAcuHxF_hGJsQjiCThFzoljtgcqvuedDtuPy', '#ffffff', NULL, NULL, 'giraffe');
+INSERT INTO `memes` (`id`, `top_quote`, `bottom_quote`, `img_url`, `font_color`, `generated_img`, `generated_data`, `animal`) VALUES (4, 'we have the best memes', 'belive me', 'https://cdn.playbuzz.com/cdn//3347d1d6-e96d-4791-870d-3ef8883c810e/bd1ca2d8-29bd-4f63-9d33-5f8c6aba4582.jpg', '#d5e83f', NULL, NULL, 'dog');
+INSERT INTO `memes` (`id`, `top_quote`, `bottom_quote`, `img_url`, `font_color`, `generated_img`, `generated_data`, `animal`) VALUES (5, NULL, NULL, 'https://i.ibb.co/q9H3f9y/Photo-on-6-8-19-at-10-07-AM-2.jpg', '#ffffff', NULL, NULL, 'dog');
+INSERT INTO `memes` (`id`, `top_quote`, `bottom_quote`, `img_url`, `font_color`, `generated_img`, `generated_data`, `animal`) VALUES (6, 'ayyyyyyyy', 'lmao', 'https://i.kym-cdn.com/entries/icons/mobile/000/014/178/AyyyLmao.jpg', '#ffffff', NULL, NULL, 'other');
+INSERT INTO `memes` (`id`, `top_quote`, `bottom_quote`, `img_url`, `font_color`, `generated_img`, `generated_data`, `animal`) VALUES (7, 'nope.', 'not today.', 'https://i.pinimg.com/originals/df/3e/47/df3e47ec566db33dbff3ec990b7f9e46.jpg', '#ffffff', NULL, NULL, 'other');
+INSERT INTO `memes` (`id`, `top_quote`, `bottom_quote`, `img_url`, `font_color`, `generated_img`, `generated_data`, `animal`) VALUES (8, 'Proof', 'earth is not flat', 'https://i.imgur.com/JP6qdcM.jpg', '#d5e83f', NULL, NULL, 'cat');
+INSERT INTO `memes` (`id`, `top_quote`, `bottom_quote`, `img_url`, `font_color`, `generated_img`, `generated_data`, `animal`) VALUES (9, NULL, NULL, 'http://geeeply.com//img/user/gif/4/1/5/3/415342172122408.gif', '#ffffff', NULL, NULL, 'giraffe');
+INSERT INTO `memes` (`id`, `top_quote`, `bottom_quote`, `img_url`, `font_color`, `generated_img`, `generated_data`, `animal`) VALUES (10, 'yaaar!', '', 'https://media3.giphy.com/media/3oEduVGyuRGuZXaqys/giphy.gif', '#c12626', NULL, NULL, 'cat');
+INSERT INTO `memes` (`id`, `top_quote`, `bottom_quote`, `img_url`, `font_color`, `generated_img`, `generated_data`, `animal`) VALUES (11, 'i belive', 'i can fly', 'https://media.giphy.com/media/FDHDP7DREKSlYtHm43/source.gif', '#f478ac', NULL, NULL, 'dog');
+INSERT INTO `memes` (`id`, `top_quote`, `bottom_quote`, `img_url`, `font_color`, `generated_img`, `generated_data`, `animal`) VALUES (12, 'bork', 'bork     bork', 'https://i.ytimg.com/vi/qSTIKkena24/maxresdefault.jpg', '#ffffff', NULL, NULL, 'dog');
 
 COMMIT;
 
